a9b201add38b6edd58e437ab7b08ad615726bef0
Tyler Martin <tyler.martin@nist.gov>
2025-02-18 23:46:26 -0500

    [chore] Remove build library artifacts from repository
    Cleans up cached build library files that are no longer needed in the repository, maintaining a cleaner project structure.

--- build/lib/AFL/double_agent_tutorial/__init__.py
+++
@@ -1,9 +0,0 @@
-from AFL.double_agent_tutorial.core import *
-import pathlib
-
-# Get the package install directory
-PACKAGE_DIR = pathlib.Path(__file__).parent.parent.parent.resolve()
-DATA_DIR = PACKAGE_DIR / "data"
-
-print(PACKAGE_DIR)
-print(DATA_DIR)
--- build/lib/AFL/double_agent_tutorial/core/__init__.py
+++
@@ -1,5 +0,0 @@
-from .plot import *
-from .util import *
-from .actively_learn import *
-from .VirtualInstrument import *
-from .util import *
